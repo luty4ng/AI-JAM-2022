b98f5554e9330de88be0e0c8cdb3f6584308797d
Trojan1019 <115767228+Trojan1019@users.noreply.github.com>
2022-12-18 07:25:24 +0800

    trojan提交

---
+++ Assets/GameMain/DialogSystem/Scripts/Scene5Listener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using GameKit;
+
+namespace Assets.GameMain.DialogSystem.Scripts
+{
+    public class Scene5Listener : MonoBehaviour
+    {
+        public void Start()
+        {
+            EventManager.instance.AddEventListener<string>(EventSettings.DIALOG_END,(string  str)=>
+            {
+                Debug.Log(str);
+            });
+        }
+
+    }
+}
---
+++ Assets/GameMain/Resources/UI/Scripts/BackgroundChange.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameKit;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class BackgroundChange : MonoBehaviour
+{
+
+}
