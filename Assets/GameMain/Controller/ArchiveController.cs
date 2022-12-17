13ebcfe60839dc606d21c1066be2e63acc6ff8f4
Trojan1019 <115767228+Trojan1019@users.noreply.github.com>
2022-12-17 10:47:02 +0800

    trojan第九次提交

---
+++ Assets/GameMain/Controller/ArchiveController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using GameKit;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ArchiveController : MonoSingletonBase<ArchiveController>
+{
+    public void SaveArichive(int archNum, int sceneID)
+    {
+        PlayerPrefs.SetInt("ArchNum="+archNum, sceneID);
+        PlayerPrefs.Save(); //保存我们设置的PlayerPrefs
+    }
+
+    public void LoadArichive(int archNum)
+    {
+        int sceneID = ArchiveController.current.GetArchive(archNum);
+        SceneController.current.GoToSceneByID(sceneID);
+    }
+    
+    public int GetArchive(int archNum)
+    {
+        return PlayerPrefs.GetInt("ArchNum=" + archNum, 404);
+
+    }
+
+
+
+
+
+}
---
+++ Assets/GameMain/Resources/UI/Scripts/ArchiveObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using GameKit;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UnityEngine.Events;
+
+namespace Assets.GameMain.Resources.UI.Scripts
+{
+    public class ArchiveObject : UIGroup
+    {
+        public int ArchiveNum;
+        public int SceneID;
+
+        public Text textArchiveNum;
+        public Text textSceneID;
+
+        protected override void OnStart()
+        {
+            textArchiveNum.text = ArchiveNum.ToString();
+            textSceneID.text = SceneID.ToString();
+        }
+    }
+}
